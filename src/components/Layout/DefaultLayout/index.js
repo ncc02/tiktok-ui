024b9d4fdfe7678f0b946262eefc1d398b9b23a2
ncc02 <cuongtk2002@gmail.com>
2024-06-04 15:56:41 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout cho dự án Tiktok

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import SideBar from "./SideBar";
+
+function DefaultLayout({children}) {
+    return ( 
+        <div>
+            <Header/>
+            <div className="container">
+                <SideBar/>
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+            
+        </div>
+     );
+}
+
+export default DefaultLayout;
